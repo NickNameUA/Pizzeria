4492be34e0f58c47e18e353cf8e2ece0ea52f82d
Bogdan Tyron <bogdan.tyron3@gmail.com>
2023-10-21 18:16:09 +0300

    Cort Hot fix (Select delay, To many reloads)

--- client/src/Components/Cort/CortItemsList.tsx
+++ client/src/Components/Cort/CortItemsList.tsx
@@ -6,29 +6,25 @@ import EmptyCort from "./EmptyCort";
 
 const CortItemsList = () => {
   const [list, setList] = useState<string[]>([]);
-  const [storage, setStorage] = useState(localStorage);
+  const [load, setLoad] = useState(false);
 
   const arr = [] as any;
 
-  useEffect(() => {
-    for (let key in storage) {
-      if (storage[key] == "true") {
+  if (!load) {
+    for (let key in localStorage) {
+      if (localStorage[key] == "true") {
         if (!arr.includes(key)) {
           arr.push(key);
           setList(arr);
         }
       }
     }
-  }, [list]);
+    setLoad(true);
+  }
 
-  setInterval(() => {
-    if (setStorage.length != storage.length) {
-      setStorage(localStorage);
-    }
-  }, 1000);
   return (
     <div id="cortItemsList">
-      {storage.length != 0 ? (
+      {localStorage.length != 0 ? (
         list.map((e: any) => {
           return <CortItem name={e} key={e} />;
         })
